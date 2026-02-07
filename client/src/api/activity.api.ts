232997d75ae0c2a5d40ac5e3346b6cf2d3004181
manojchauhan33 <manojchauhan9963@gmail.com>
2026-02-07 17:36:05 +0530

    fix ui in frontend activity page api , and config

--- client/src/api/activity.api.ts
+++ client/src/api/activity.api.ts
@@ -10,14 +10,6 @@ export interface ActivityRecord {
   createdAt: string;
 }
 
-// export const ActivityAPI = {
-//   getAll: async (): Promise<ActivityRecord[]> => {
-//     const res = await axiosClient.get("/api/activity");
-//     return res.data.activities;
-//   }
-// };
-
-// activity.api.ts
 export const ActivityAPI = {
   getAll: async (page: number = 1): Promise<{ activities: ActivityRecord[], totalPages: number }> => {
     const res = await axiosClient.get(`/api/activity?page=${page}&limit=5`);
--- client/src/config/env.ts
+++ client/src/config/env.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 export const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
